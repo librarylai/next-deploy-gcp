d3e400fdce82a26ee554e0d2e2c6ffb077f25fec
librarylai <library0515@gmail.com>
2023-09-01 21:46:22 +0800

    feat: add Testing

---
+++ __tests__/sample.js
@@ -0,0 +1,5 @@
+describe('Sample', () => {
+  it('one plus one', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
---
+++ jest.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  collectCoverage: true,
+  // on node 14.x coverage provider v8 offers good speed and more or less good report
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!./out/**',
+    '!./.next/**',
+    '!./*.config.js',
+    '!./coverage/**',
+  ],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': './__mocks__/styleMock.js',
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `./__mocks__/fileMock.js`,
+
+    // Handle module aliases
+    '^@/components/(.*)$': './src/components/$1',
+  },
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['./jest.setup.js'],
+  testPathIgnorePatterns: ['./node_modules/', './.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+}
